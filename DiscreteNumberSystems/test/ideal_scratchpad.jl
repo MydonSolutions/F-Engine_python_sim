b11dae1c9335aed00ce2d25167e09f02c25ce310
talonmyburgh <myburgh.talon@gmail.com>
2022-03-22 11:23:18 +0200

    *+ Added CNArray and CScalar. - All getindex and setindex operations seem to work.

--- DiscreteNumberSystems/test/ideal_scratchpad.jl
+++ DiscreteNumberSystems/test/ideal_scratchpad.jl
@@ -1,14 +1,42 @@
-struct NArray{N} <: AbstractArray{Int, N}
-	data::Array{Int, N}
+struct NArray{N} <: AbstractArray{Integer, N}
+	data::Array{Integer, N}
 end
 struct Scalar
-	data::Int
+	data::Integer
 end
+
+struct CScalar
+	real::Integer
+	imag::Integer
+end
+struct CNArray{N} <:AbstractArray{Complex{<:Integer},N}
+	real :: NArray{N}
+	imag :: NArray{N}
+	function CNArray{N}(real :: NArray{N}, imag :: NArray{N}) where {N}
+		return new{N}(real,imag)
+	end
+end
+
+CNArray(real::Scalar,imag::Scalar) = CScalar(real,imag)
+CNArray(real::Integer,imag::Integer) = CScalar(Scalar(real),Scalar(imag))
 
 NArray(data::Integer) = Scalar(data)
 
 Base.size(A::NArray) = size(A.data)
 Base.size(A::Scalar) = size(A.data)
+Base.size(CA::CNArray) = size(CA.real.data)
+Base.size(CS::CScalar) = size(CS.real)
+
+Base.@inline function Base.getindex(CA::CNArray{N}, i::Vararg{Integer,N}) where {N}
+	#Assume that real.data has the same bounds as imag.data
+	@boundscheck checkbounds(CA.real.data, i...)
+	@inbounds CNArray(getindex(CA.real, i...),getindex(CA.imag, i...))
+end
+Base.@inline function Base.setindex!(CA::CNArray{N},v, i::Vararg{Int,N}) where{N}
+	#Assume that real.data has the same bounds as imag.data
+	@boundscheck checkbounds(CA.real.data,i...)
+	@inbounds CNArray{N}(setindex!(CA.real,v.real,i...),setindex!(CA.imag,v.imag,i...))
+end
 
 Base.@inline function Base.getindex(A::NArray{N}, i::Vararg{Int, N}) where {N}
 	@boundscheck checkbounds(A.data, i...)
@@ -21,14 +49,21 @@ Base.@inline function Base.setindex!(A::NArray{N}, v, i::Vararg{Int, N}) where {
 end
 
 Base.convert(::Type{<:Integer}, arr::Scalar) = arr.data
+Base.convert(::Type{<:Complex{<:Integer}}, arr::CScalar) = arr.real + 1im* arr.imag
 
 function Base.show(io::IO, v::NArray)
 	print(v.data)
+end
+function Base.show(io::IO, v::CNArray)
+	print(v.real,v.imag)
 end
 
 using Random
 
 a = NArray{3}(Random.rand(Int, 3, 3, 3))
+s = NArray{1}(Random.rand(Int,2))
+
+ca = CNArray{3}(a,a)
 println("a:\n", a, "\n")
 println("a.data[3,3,3]:\n", a.data[3,3,3], "\n")
 println("a[3,3,3]:\n", a[3,3,3], "\n")
@@ -39,5 +74,20 @@ println("eachindex(a):\n", eachindex(a), "\n")
 println("map(println, a):\n")
 map(println, a)
 println("\n")
-
+println("s: ",s[:])
+a[3,1:2,3] = s[:]
+println("a[3,1:2,3] = s[:]",a[3,1:2,3])
+println("a[3,3,2:end]",a[3,3,2:end])
 println("hcat(a[1:2, :, :], Random.rand(Int, 2, 3, 3)):\n", hcat(a[1:2, :, :], Random.rand(Int, 2, 3, 3)), "\n")
+println("ca:\n",ca,"\n")
+println("ca.real.data[3,3,3]:\n", ca.real.data[3,3,3], "\n")
+println("ca.imag.data[3,3,3]:\n", ca.imag.data[3,3,3], "\n")
+println("ca[3,3,3]:\n", ca[3,3,3], "\n")
+println("ca.real[3,3,:]:\n", ca.real[3,3,:], "\n")
+println("ca[3,3,:]:\n", ca[3,3,:], "\n")
+println("ca[3,1:2:3,:] .+ 1:\n", ca[3,1:2:3,:] .+ 1, "\n")
+println("eachindex(ca):\n", eachindex(ca), "\n")
+println("map(println, ca):\n")
+map(println, ca)
+println("\n")
+println("hcat(ca[1:2, :, :], Random.rand(Complex{Int}, 2, 3, 3)):\n", hcat(ca[1:2, :, :], Random.rand(Complex{Int}, 2, 3, 3)), "\n")
